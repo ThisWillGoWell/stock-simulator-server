d861b087e0943431c6b8f8ead35a8b50efe1d6f6
ThisWillGoWell <william.gowell@gmail.com>
2018-01-14 13:25:51 -0800

    run app now uses clients

--- app/app.go
+++ app/app.go
@@ -20,24 +20,57 @@ const ledgerUpdateName  = "ledger"
 const stockUpdateName = "stock"
 
 func RunApp(){
+	stock1, _:= valuable.NewStock("CHUNT", "Chunt's Hats", 69,  time.Second * 60)
+	stock2, _:= valuable.NewStock("KING", "Paddle King", 10,  time.Second * 5)
+	stock3, _:= valuable.NewStock("CBIO", "Sebio's Streaming Services", 10,  time.Second * 30)
+	valuable.StartStockStimulation()
+	//Make an exchange
+	exchanger, _ := exchange.BuildExchange("US")
+	exchanger.StartExchange()
+	//Register stocks with Exchange
+	exchanger.RegisterValuable(stock1, 100)
+	exchanger.RegisterValuable(stock2, 100)
+	exchanger.RegisterValuable(stock3, 100)
+
+	/*
 	txMessage := utils.MakeDuplicator()
 
 	portfolioMessageBuilder := utils.MakeDuplicator()
 	stockMessageBuilder := utils.MakeDuplicator()
 	ledgerMessageBuilder := utils.MakeDuplicator()
 
-	//something that take the portfolio update and turns it into a portfolio Update message
+	//something that take the updates andturns it into a Update message
+	go func(){
+
+		portfolio := portfolioMessageBuilder.GetOutput()
+		stock := stockMessageBuilder.GetOutput()
+		ledger := ledgerMessageBuilder.GetOutput()
+		for{
+			var update interface{}
+			var updateType string
+			select{
+			case update = <- portfolio:
+				updateType = portfolioUpdateName
+			case update = <- stock:
+				updateType = stockUpdateName
+			case  update = <- ledger:
+				updateType = ledgerUpdateName
+			}
+			txMessage.Offer(messages.BuildUpdateMessage(updateType, update))
+		}
+
+	}()
 	go func(){
 		updateChannel := portfolioMessageBuilder.GetOutput()
 		for update := range updateChannel{
-			txMessage.Offer(messages.NewUpdateMessage(portfolioUpdateName, update))
+			txMessage.Offer(messages.BuildUpdateMessage(portfolioUpdateName, update))
 			}
 	}()
 	//something that take the stock update and turns it into a portfolio Update message
 	go func(){
 		updateChannel := stockMessageBuilder.GetOutput()
 		for update := range updateChannel{
-			txMessage.Offer(messages.NewUpdateMessage(stockUpdateName, update))
+			txMessage.Offer(messages.BuildUpdateMessage(stockUpdateName, update))
 		}
 	}()
 
@@ -45,7 +78,7 @@ func RunApp(){
 	go func(){
 		updateChannel := ledgerMessageBuilder.GetOutput()
 		for update := range updateChannel{
-			txMessage.Offer(messages.NewUpdateMessage(ledgerUpdateName, update))
+			txMessage.Offer(messages.BuildUpdateMessage(ledgerUpdateName, update))
 		}
 	}()
 
@@ -87,20 +120,21 @@ func RunApp(){
 	portfolioMessageBuilder.RegisterInput(port.UpdateChannel.GetOutput())
 	portfolioMessageBuilder.RegisterInput(port2.UpdateChannel.GetOutput())
 	//make a purchase order
-	po := order.BuildPurchaseOrder(valuable.Valuables["CHUNT"], port, 10)
-	exchange.Exchanges["US"].InitiateTrade(po)
+	po := order.BuildPurchaseOrder("CHUNT", "US", "1", 10)
+	exchange.InitiateTrade(po)
 	time.Sleep(2 * time.Second)
-	po2 := order.BuildPurchaseOrder(valuable.Valuables["KING"], port, 5)
-	exchange.Exchanges["US"].InitiateTrade(po2)
+	po2 := order.BuildPurchaseOrder("KING", "US", "1", 5)
+	exchange.InitiateTrade(po2)
 	time.Sleep(2 * time.Second)
-	po3 := order.BuildPurchaseOrder(valuable.Valuables["CBIO"], port, 1)
-	exchange.Exchanges["US"].InitiateTrade(po3)
+	po3 := order.BuildPurchaseOrder("CBIO", "US", "1", 1)
+	exchange.InitiateTrade(po3)
 	time.Sleep(2 * time.Second)
-	po4 := order.BuildPurchaseOrder(valuable.Valuables["CBIO"], port2, 1)
-	exchange.Exchanges["US"].InitiateTrade(po4)
+	po4 := order.BuildPurchaseOrder("CBIO", "US", "2", 1)
+	exchange.InitiateTrade(po4)
 	time.Sleep(2 * time.Second)
 
 	result := <- po.ResponseChannel
 	fmt.Println(result)
+		*/
 
 }
