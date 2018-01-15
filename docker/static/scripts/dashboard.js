04abb70d59481d25fc58058892acbee0ad8355e5
ThisWillGoWell <william.gowell@gmail.com>
2018-01-15 01:04:41 -0800

    ws uri is now pulled from the host

--- docker/static/scripts/dashboard.js
+++ docker/static/scripts/dashboard.js
@@ -59,13 +59,17 @@ $( document ).ready(function() {
 
 	var chat_feed = $('#chat-module--container .chat-message--list');
 
-	function appendNewMessage(msg){
+	function appendNewMessage(msg, fromMe){
 
+		let isMe = "";
+		if (fromMe) {
+			isMe = "is-me";
+		}
 		let msg_text = msg.body;
 		let msg_author = msg.author;
 		let msg_timestamp = msg.timestamp;
 		let msg_template = '<li>'+
-				'				<div class="msg-username">'+ msg_author +' <span class="msg-timestamp">'+ msg_timestamp +'</span></div>'+
+				'				<div class="msg-username '+ isMe +'">'+ msg_author +' <span class="msg-timestamp">'+ msg_timestamp +'</span></div>'+
 				'				<div class="msg-text">'+ msg_text +'</div>'+
 				'			</li>';
 
@@ -94,7 +98,7 @@ $( document ).ready(function() {
 			chat_feed.empty();
 		}
 
-	    appendNewMessage(sampleMessages[i]);
+	    appendNewMessage(sampleMessages[i], false);
 
 	    i++;
 
@@ -105,6 +109,20 @@ $( document ).ready(function() {
         $('#chat-module--container').toggleClass('closed');
         $('#chat-text-input').focus();
     });
+
+    $('#top-bar--container .account-settings-btn').click(function() {
+    
+        $('#top-bar--container .account-settings-menu--container').toggleClass('open');
+        
+    });
+
+    $('#account-settings-menu-close-btn').click(function() {
+    
+        $('#top-bar--container .account-settings-menu--container').toggleClass('open');
+        
+    });
+
+    
 
 
 
@@ -119,7 +137,7 @@ $( document ).ready(function() {
 			        timestamp: timestamp,
 			        body: message_body,
 			    };
-		        appendNewMessage(temp_msg);
+		        appendNewMessage(temp_msg, true);
 		        $('#chat-module--container textarea').val().replace(/\n/g, "");
 		        $('#chat-module--container textarea').val('');
 		        return false;
