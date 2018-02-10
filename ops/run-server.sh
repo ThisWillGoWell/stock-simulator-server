2f442dfbbb498342f78ca2d67efc608d0f73f758
Will <william.gowell@gmail.com>
2018-02-10 13:15:50 -0800

    fixed static serve folder

--- ops/run-server.sh
+++ ops/run-server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd docker
-export STATIC_FOLDER=/root/starket/mockstarket-front-end
+export STATIC_FOLDER=/root/starket/stock-simulator-server/mockstarket-front-end
 docker-compose build
 docker-compose up  -d server
