6329df81808e3a9b823235810fb5b1c2e4619af6
will <william.gowell@gmail.com>
2018-03-13 22:48:56 -0700

    added run script for local

--- ops/run-server.sh
+++ ops/run-server.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
+
 cd docker
-export STATIC_FOLDER=/root/starket/stock-simulator-server/mockstarket-front-end
-docker-compose down server
+
+if [ "$HOSTNAME" = "high-in-the-clouds" ]; then
+    export STATIC_FOLDER=/root/starket/stock-simulator-server/mockstarket-front-end
+else
+    export FILE_SERVE=$(cd .. && pwd cd docker)/debug_frontend
+fi
+echo $FILE_SERVE
+docker-compose rm -f server
 docker-compose build
 docker-compose up -d server
