ae159a659036cb9be6745ed5db145c1439ae3e50
will <william.gowell@gmail.com>
2018-03-29 22:30:33 -0700

    gofmt

--- src/account/user.go
+++ src/account/user.go
@@ -2,10 +2,10 @@ package account
 
 import (
 	"errors"
+	"github.com/stock-simulator-server/src/change"
+	"github.com/stock-simulator-server/src/lock"
 	"github.com/stock-simulator-server/src/portfolio"
 	"github.com/stock-simulator-server/src/utils"
-	"github.com/stock-simulator-server/src/lock"
-	"github.com/stock-simulator-server/src/change"
 )
 
 // keep the uuid to user
@@ -15,16 +15,24 @@ var userList = make(map[string]*User)
 var uuidList = make(map[string]string)
 var userListLock = lock.NewLock("user-list")
 
+/*
+User Object
+Represents a unique individual of the system
+*/
 type User struct {
-	UserName      string `json:"-"`
-	password      string `json:"-"`
-	DisplayName   string `json:"display_name" change:"-"`
-	Uuid          string `json:"-"`
-	Active        bool   `json:"active" change:"-"`
-	ActiveClients int64 `json:"-"`
-	Lock 		  *lock.Lock `json:"-"`
+	UserName      string     `json:"-"`
+	password      string     `json:"-"`
+	DisplayName   string     `json:"display_name" change:"-"`
+	Uuid          string     `json:"-"`
+	Active        bool       `json:"active" change:"-"`
+	ActiveClients int64      `json:"-"`
+	Lock          *lock.Lock `json:"-"`
 }
 
+/**
+Return a user provided the username and password
+If the password is correct return user, else return err
+*/
 func GetUser(username, password string) (*User, error) {
 	userListLock.Acquire("get-user")
 	defer userListLock.Release()
@@ -41,11 +49,15 @@ func GetUser(username, password string) (*User, error) {
 	return user, nil
 }
 
-func NewUser(username, password string) (*User, error){
+/**
+Build a new user
+set their password to that provided
+*/
+func NewUser(username, password string) (*User, error) {
 	userListLock.Acquire("new-user")
 	defer userListLock.Release()
 	_, userNameExists := uuidList[username]
-	if userNameExists{
+	if userNameExists {
 		return nil, errors.New("username already exists")
 	}
 	uuid := utils.PseudoUuid()
@@ -60,8 +72,8 @@ func NewUser(username, password string) (*User, error){
 				DisplayName: username,
 				password:    password,
 				Uuid:        uuid,
-				Lock: 		 lock.NewLock("user"),
-				Active: 	 true,
+				Lock:        lock.NewLock("user"),
+				Active:      true,
 			}
 			change.SubscribeUpdateInputs.Offer(userList[uuid])
 			return userList[uuid], nil
@@ -73,34 +85,40 @@ func NewUser(username, password string) (*User, error){
 	return nil, nil
 }
 
-func (user *User) LogoutUser(){
+/**
+Logout the user and decrement the active client count
+*/
+func (user *User) LogoutUser() {
 	user.Lock.Acquire("logout")
 	defer user.Lock.Release()
 	user.ActiveClients -= 1
-	if user.ActiveClients < 0{
+	if user.ActiveClients < 0 {
 		user.ActiveClients = 0
 	}
-	if user.ActiveClients == 0{
+	if user.ActiveClients == 0 {
 		user.Active = false
 	}
 	change.SubscribeUpdateInputs.Offer(user)
 }
 
-func (user *User)GetId() string {
+func (user *User) GetId() string {
 	return user.Uuid
 }
 func (user *User) GetType() string {
 	return "user"
 }
 
-func GetAllUsers()[]*User{
+/**
+Turn the user map into a list so they can be sent to a rx client
+*/
+func GetAllUsers() []*User {
 	userListLock.Acquire("get all users")
 	defer userListLock.Release()
 	lst := make([]*User, len(userList))
 	i := 0
-	for _, val := range userList{
+	for _, val := range userList {
 		lst[i] = val
-		i+= 1
+		i += 1
 	}
 	return lst
 }
