47edd38f990c0b69ea46ef038bd319edaef5df27
thiswillgowell <william.gowell@gmail.com>
2018-08-30 14:51:07 -0400

    message

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -3,7 +3,7 @@ message=$1
 echo git commit -a -m "$message"
 git commit -a -m "message"
 echo 'ssh'
-command="cd /root/starket/stock-simulator-server && git status"
+command="git -C /root/starket/stock-simulator-server status"
 run="cd /root/starket/stock-simulator-server && echo here && ./etc/build.sh"
 ssh -t root@159.89.154.221 "$command"
 ssh -t root@159.89.154.221 "$run"
