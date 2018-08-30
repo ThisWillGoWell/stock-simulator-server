036825cdbc94751284e1af0319fd3c34201aedfc
thiswillgowell <william.gowell@gmail.com>
2018-08-30 14:45:22 -0400

    message

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -5,5 +5,5 @@ git commit -a -m "message"
 echo 'ssh'
 command="cd starket/stock-simulator-server && git pull"
 run="cd /root/starket/stock-simulator-server && echo here && ./etc/build.sh"
-ssh root@159.89.154.221 $command
-ssh root@159.89.154.221 $run
+ssh root@159.89.154.221 "$command"
+ssh root@159.89.154.221 "$run"
