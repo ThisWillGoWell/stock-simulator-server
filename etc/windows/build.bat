f9b1dd8174396fbbf1bf395ee6c11dee48dc25bb
William.Gowell <William.Gowell>
2018-09-03 15:57:01 -0400

    frontend added

--- etc/windows/build.bat
+++ etc/windows/build.bat
@@ -3,6 +3,5 @@ set GOARCH=amd64
 set GOOS=linux
 go build
 copy stock-simulator-server docker\stock-server-linux
-docker-compose build --no-cache
 del stock-simulator-server
 
--- etc/windows/run.bat
+++ etc/windows/run.bat
@@ -1,5 +1,5 @@
 @echo off
-set FILE_SERVE=c:/Users/Willi/Dropbox/workspace/go/src/github.com/stock-simulator-server/debug_frontend
+set FILE_SERVE=c:/Users/Willi/Dropbox/workspace/go/src/github.com/stock-simulator-server/front_end
 docker-compose up -d
 docker-compose logs -f
 
