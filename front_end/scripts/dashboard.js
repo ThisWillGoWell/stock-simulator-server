735c504aca1dfd48c7ee5ba2b3c83a2332141525
Jakob Elias <jelias1@luc.edu>
2018-09-21 13:22:02 -0500

    price fixing

--- front_end/scripts/dashboard.js
+++ front_end/scripts/dashboard.js
@@ -217,25 +217,7 @@ if(authenticated) {
 				},
 			}
 		});
-
-		// // FIX THIS
-		// var formatPrice = function(value) {
-		// 	// TODO if value is greater than something abbreviate
-		// 	if (value < 1000000) {
-		// 		let val = (value/100).toFixed(2).toString();
-		// 		val = val.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-		// 		return val;
-		// 	} else if (value < 100000000) {
-		// 		let val = ((value/100)/1000).toFixed(2).toString();
-		// 		val = val.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-		// 		return val + "K";
-		// 	} else {
-		// 		let val = (value/100).toFixed(2).toString();
-		// 		val = val.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-		// 		return val;
-		// 	}
-	    // }
-
+		
 
 	    var currUser = new Vue({
 	    	el: '#dashboard--view',
