269619642733cb1ff0f364d2799e2b14c71887a7
William.Gowell <William.Gowell>
2018-09-26 20:51:32 -0700

    added displayName to allow header

--- src/web/http.go
+++ src/web/http.go
@@ -124,7 +124,7 @@ var upgrader = websocket.Upgrader{
 func setupResponse(w *http.ResponseWriter, req *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	(*w).Header().Set("Access-Control-Allow-Headers", "DisplayName, Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
 
