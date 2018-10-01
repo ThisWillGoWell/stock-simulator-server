f41abbd85bf965659b587faede63b089c18a6b97
Jakob Elias <jelias1@luc.edu>
2018-10-01 00:11:35 -0500

    auth checking moved to websocket file

--- front_end/scripts/websocket.js
+++ front_end/scripts/websocket.js
@@ -1,68 +1,79 @@
-/*  WEBSOCKETS */
-var externalServer = "mockstarket.com";
-var localServer = window.location.host;
-var wsUri = "wss://"+ externalServer + "/ws";
-var output;
-var webSocket;
+// ADDED THIS BLOCK FOR AUTH - TELL JAKE
+var token = sessionStorage.getItem('token');
+var auth_uuid = sessionStorage.getItem('uuid');
 
-var router = {};
+if(token) {
+	/*  WEBSOCKETS */
+	var externalServer = "mockstarket.com";
+	var localServer = window.location.host;
+	var wsUri = "wss://"+ externalServer + "/ws";
+	var output;
+	var webSocket;
 
+	var router = {};
 
-function init() {
-    webSocket = new WebSocket(wsUri);
-    webSocket.onopen = function(evt) { onOpen(evt) };
-    webSocket.onclose = function(evt) { onClose(evt) };
-    webSocket.onmessage = function(evt) { onMessage(evt) };
-    webSocket.onerror = function(evt) { onError(evt) };
-};
+	function init() {
+	    webSocket = new WebSocket(wsUri);
+	    webSocket.onopen = function(evt) { onOpen(evt) };
+	    webSocket.onclose = function(evt) { onClose(evt) };
+	    webSocket.onmessage = function(evt) { onMessage(evt) };
+	    webSocket.onerror = function(evt) { onError(evt) };
+	};
 
-function onOpen(evt) {
-	if (sessionStorage.getItem('token') !== null) {
-        var loginMessage = {
-            'action': 'connect',
-            'msg': {
-                'token': sessionStorage.getItem('token')
-            }
-        };
-        doSend(JSON.stringify(loginMessage));
-    }   
-	onEvent("Connected");
-};
+	function onOpen(evt) {
+		if (sessionStorage.getItem('token') !== null) {
+	        var loginMessage = {
+	            'action': 'connect',
+	            'msg': {
+	                'token': sessionStorage.getItem('token')
+	            }
+	        };
+	        doSend(JSON.stringify(loginMessage));
+	    }   
+		onEvent("Connected");
+	};
 
-function onClose(evt) {
-    onEvent("Disconnected");
-};
+	function onClose(evt) {
+	    onEvent("Disconnected");
+	};
 
-function onEvent(message){
-    console.log(message);
-};
+	function onEvent(message){
+	    console.log(message);
+	};
 
-function onMessage(evt) {
-    var msg = JSON.parse(evt.data);
-    // console.log(msg);
-	
-	if (msg.action) {
-		router[msg.action](msg);
-	} else {
-		if (msg.type == "error") {
-			console.log("ERROR")
-			console.log(msg);
+	function onMessage(evt) {
+	    var msg = JSON.parse(evt.data);
+	    // console.log(msg);
+		
+		if (msg.action) {
+			router[msg.action](msg);
+		} else {
+			if (msg.type == "error") {
+				console.log("ERROR")
+				console.log(msg);
+			}
+			console.log("No message action");
+			console.log(console.log(msg));	
 		}
-		console.log("No message action");
-		console.log(console.log(msg));	
-	}
-};
+	};
 
-function onError(evt) {
-	console.log(evt);
-    // writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
-};
+	function onError(evt) {
+		console.log(evt);
+	    // writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
+	};
 
-function doSend(message) {
-    webSocket.send(message);
-};
+	function doSend(message) {
+	    webSocket.send(message);
+	};
 
-function registerRoute(route, callback) {
-	router[route] = callback;
-};
+	function registerRoute(route, callback) {
+		router[route] = callback;
+	};
 
+	init();
+
+} else {
+
+	window.location.href = "/login.html";
+
+}
