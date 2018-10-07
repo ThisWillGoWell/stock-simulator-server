1e6214a21a7a51d1ae3f2f39274dbde70f649593
Elias <jelias@anl.gov>
2018-10-07 14:43:07 -0500

    rework of dosend and sound

--- front_end/scripts/index.js
+++ front_end/scripts/index.js
@@ -93,12 +93,12 @@ registerRoute("alert", function(msg) {
 
 
 $(document).ready(function() {
+  load_notifications(); // notifications.js
   load_dashboard_tab(); // dashboard.js
   load_investors_tab(); // investors.js
   load_stocks_tab(); // stocks.js
   load_store_tab(); // store.js
   load_topbar_vue(); // topbar.js
-  load_notifications(); // notifications.js
   load_sidebar_vue(); // sidebar.js
   load_chat_vue(); // chat.js
   load_modal_vues(); // modal.js
@@ -134,10 +134,6 @@ $(document).ready(function() {
       }
     }
   });
-
-
-  var notification_sound = new Audio();
-  notification_sound.src = "assets/sfx_pling.wav";
 
 
   $(document).scroll(function() {
@@ -352,9 +348,4 @@ function renderContent(route) {
     }
 }
 
-  // SOUND EFFECTS
-
-  var notification_sound = new Audio();
-  notification_sound.src = "assets/sfx_pling.wav";
-  notification_sound.volume = 0.2;
 });
