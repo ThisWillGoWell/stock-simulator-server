7aa441ab4d0a01b6884c12a7a98fdb3f84d394c3
William.Gowell <William.Gowell>
2018-11-02 23:27:46 -0700

    wip

--- src/effect/bonus.go
+++ src/effect/bonus.go
@@ -9,30 +9,45 @@ import (
 type BonusTradingType struct {
 }
 
-func (*BonusTradingType) Name() string {
+func (BonusTradingType) Name() string {
 	return "bonus_trading"
 }
-func (*BonusTradingType) Cost() int64 {
+func (BonusTradingType) Cost() int64 {
 	return 0
 }
 
-func (*BonusTradingType) RequiredLevel() int64 {
+func (BonusTradingType) RequiredLevel() int64 {
 	return 0
 }
 
 type BonusTrading struct {
-	Duration  utils.Duration `json:"duration"`
-	StartTime time.Time      `json:"start_time"`
+	BonusAmount float64        `json:"bonus"`
 }
 
 func (bonus *BonusTrading) IsPermanent() bool {
 	return true
 }
 
-func (bonus *BonusTrading) StartTime() int64 {
-	return bonus.StartTime
+func NewBonusTrading(portfolioUuid, title string, amount float64, duration utils.Duration) error{
+	EffectLock.Acquire("new-bonus-trading")
+	defer EffectLock.Release()
+
+	newEffect := &Effect{
+		Uuid: utils.SerialUuid(),
+		Title: title,
+		Active: true,
+		StartTime: time.Now(),
+		Type: BonusTradingType{},
+		InnerEffect: &BonusTrading{
+			BonusAmount: amount,
+		},
+	}
+
+	effects, ok := PortfolioEffects[portfolioUuid]
+	if !ok{
+		n
+	}
+	return nil
 }
 
-func (*BonusTrading) GetType() EffectType {
-	return &BonusTradingType{}
-}
+func
--- src/effect/effects.go
+++ src/effect/effects.go
@@ -2,7 +2,30 @@ package effect
 
 import (
 	"time"
+
+	"github.com/stock-simulator-server/src/lock"
+	"github.com/stock-simulator-server/src/utils"
 )
+
+var EffectLock = lock.NewLock("EffectLock")
+var PortfolioEffects = make(map[string]map[string]*Effect)
+
+// Calculate the total bonus  for a portfolio
+func TotalBonus(portfolioUuid string) float64 {
+	EffectLock.Acquire("TotalBonus")
+	effect, ok := PortfolioEffects[portfolioUuid]
+	if !ok {
+		return 0
+	}
+	totalBonus := 0.0
+	for _, e := range effect {
+		switch e.InnerEffect.(type) {
+		case BonusTradingType:
+			totalBonus += e.InnerEffect.(*BonusTrading).BonusAmount
+		}
+	}
+	return totalBonus
+}
 
 type EffectType interface {
 	Name() string
@@ -10,12 +33,14 @@ type EffectType interface {
 	RequiredLevel() int64
 }
 
-type Effect interface {
-	Active() bool
-	Duration() time.Duration
-	StartTime() time.Time
-	Type() EffectType
-	Start()
-	PortfolioUuid() string
-	GetUuid() string
+type Effect struct {
+	PortfolioUuid string         `json:"portfolio_uuid"`
+	Uuid          string         `json:"uuid"`
+	Title         string         `json:"title"`
+	Active        bool           `json:"active"`
+	IsPublic      bool           `json:"public"`
+	Duration      utils.Duration `json:"duration"`
+	StartTime     time.Time      `json:"time"`
+	Type          EffectType     `json:"effect"`
+	InnerEffect   interface{}
 }
