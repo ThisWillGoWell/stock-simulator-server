e84ba24501034ae9718a1c13b49c0b9de05f70da
wggowell <wggowell@justin.tv>
2019-10-30 00:53:36 -0700

    buildspec

--- etc/setupserver.sh
+++ etc/setupserver.sh
@@ -1,12 +1,21 @@
+#!/bin/bash
+yum -y update
 sudo yum update -y
 sudo yum install -y docker
 sudo yum install postgresql postgresql-server postgresql-devel postgresql-contrib
 sudo usermod -aG docker ec2-user
 sudo service docker start
+sudo yum install jq
+# install aws cli
+sudo yum install -y ruby
+cd /home/ec2-user
+curl -O https://aws-codedeploy-us-west-2.s3.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
 
 
 
-yum install jq
+
 
 
 
